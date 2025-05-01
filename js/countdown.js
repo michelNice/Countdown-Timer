711fa5ae424bc2fd9092f93c1dc023b8b7daba0f
michel <michel-gouveia1997@hotmail.com>
2025-05-01 06:30:37 -0300

    update

--- js/countdown.js
+++ js/countdown.js
@@ -1,63 +1,127 @@
+/*const day = document.querySelector('.day')
+const hour = document.querySelector('.hour')
+const minute = document.querySelector('.minute')
+const second = document.querySelector('.second')
+
+function ConvertML(dataOne,dataTwo){
+
+  const diffData = Math.abs(dataTwo.getTime() - dataOne.getTime());
+
+  let days,hours, minutes,seconds;
+
+  days = Math.floor(diffData / (1000 * 60 * 60 * 24));
+  hours = Math.floor((diffData % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  minutes = Math.floor((diffData % (1000 * 60 * 60)) / (1000 * 60));
+  seconds =  Math.floor((diffData % (1000 * 60)) / 1000);
+
+
+  console.log(days,hours,minutes,seconds)
+
+  return{
+    days,
+    hours,
+    minutes,
+    seconds
+  }
+}
+
+const firstDate = new Date('2025-05-01T05:26:00');
+const lastDate = new Date('2026-04-23T12:30:45');
+
+const result = ConvertML(new Date(firstDate), new Date(lastDate))
+
+
+function countdown(targetDate){
+
+  const intervaled = setInterval(()=> {
+
+    const currurent = new Date()
+
+    const remaining = ConvertML(currurent,targetDate)
+
+    day.textContent = remaining.days
+
+    hour.textContent = remaining.hours
+
+    minute.textContent = remaining.minutes
+
+    second.textContent = remaining.seconds
+
+    if(remaining.days <= 0 && remaining.hours <= 0 && remaining.minutes <= 0 && remaining.seconds <= 0){
+
+      clearInterval(intervaled)
+    }
+  },1000)
+}
+
+
+countdown(lastDate)
+
+*/
+
+
+
 const day = document.querySelector('.day')
 const hour = document.querySelector('.hour')
 const minute = document.querySelector('.minute')
 const second = document.querySelector('.second')
 
-function diffMS(dateOne,dateTwo){ 
 
-  const diff =  Math.abs(dateTwo.getTime() - dateOne.getTime());
+function convertMl(dataOne, dateTwo){
 
-  let days,hours,minutes,seconds;
+ ///why this line doesn't work, that was support to?
+ const diffDate = Math.abs(dateTwo.getTime() - dataOne.getTime())
 
-  days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  seconds =  Math.floor((diff % (1000 * 60)) / 1000);
-  console.log(days)
+  let days, hours,minutes,seconds;
 
-  return {
-     days,
-     hours,
-     minutes,
-     seconds
-  }
+   days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+   hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+   seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
+
+   console.log(days,hours,minutes,seconds)
+
+
+   return {
+    days,
+    hours,
+    minutes,
+    seconds
+   }
 
 }
-const firstDate = new Date('2025-04-30T10:00:00');
-const lastDate = new Date('2026-04-23T12:30:45');
+const firstDate = ('2025-05-01T05:48:00');
+const lastDate = ('2026-04-23T12:30:45');
 
 
-const result =  diffMS(firstDate,lastDate)
+const result = convertMl(new Date(firstDate), new Date(lastDate))
+
+function countdown(target){
 
 
+  const interved = setInterval(()=> {
 
-//nao consigo pensa em nada
-function countdown(targetDate){
+    let current = new Date()
 
+  const remaning = convertMl(current,target)
 
-  const intervaled = setInterval(()=> {
+    day.textContent = remaning.days
+    hour.textContent = remaning.hours
+    minute.textContent = remaning.minutes
+    second.textContent = remaning.seconds
 
-    const currurent = new Date();
+    if(remaning.days <= 0 && remaning.hours <= 0 && remaning.minutes <= 0 && remaning.seconds <= 0 ){
 
-    const remaining = diffMS(currurent,targetDate)
+      clearInterval(interved)
 
-    day.textContent = remaining.days
-    hour.textContent = remaining.hours
-    minute.textContent = remaining.minutes
-    second.textContent = remaining.seconds
-    
-    count--
-     if(remaining.days <= 0 && remaining.hours <= 0 && remaining.minutes <= 0 && remaining.seconds <= 0){
-
-      clearInterval(intervaled)
-     }
+    }
   },1000)
+
+
 }
 
-countdown(lastDate)
 
-
-
+countdown(new Date(lastDate))
 
 
 
