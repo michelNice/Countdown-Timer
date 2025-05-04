e505a471d16b4cba67d7bf14e94912d53d55301c
michel <michel-gouveia1997@hotmail.com>
2025-05-04 05:26:20 -0300

    update

--- js/countdown.js
+++ js/countdown.js
@@ -1,65 +1,3 @@
-/*
-/*const day = document.querySelector('.day')
-const hour = document.querySelector('.hour')
-const minute = document.querySelector('.minute')
-const second = document.querySelector('.second')
-
-function ConvertML(dataOne,dataTwo){
-
-  const diffData = Math.abs(dataTwo.getTime() - dataOne.getTime());
-
-  let days,hours, minutes,seconds;
-
-  days = Math.floor(diffData / (1000 * 60 * 60 * 24));
-  hours = Math.floor((diffData % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  minutes = Math.floor((diffData % (1000 * 60 * 60)) / (1000 * 60));
-  seconds =  Math.floor((diffData % (1000 * 60)) / 1000);
-
-
-  console.log(days,hours,minutes,seconds)
-
-  return{
-    days,
-    hours,
-    minutes,
-    seconds
-  }
-}
-
-const firstDate = new Date('2025-05-01T05:26:00');
-const lastDate = new Date('2026-04-23T12:30:45');
-
-const result = ConvertML(new Date(firstDate), new Date(lastDate))
-
-
-function countdown(targetDate){
-
-  const intervaled = setInterval(()=> {
-
-    const currurent = new Date()
-
-    const remaining = ConvertML(currurent,targetDate)
-
-    day.textContent = remaining.days
-
-    hour.textContent = remaining.hours
-
-    minute.textContent = remaining.minutes
-
-    second.textContent = remaining.seconds
-
-    if(remaining.days <= 0 && remaining.hours <= 0 && remaining.minutes <= 0 && remaining.seconds <= 0){
-
-      clearInterval(intervaled)
-    }
-  },1000)
-}
-
-
-countdown(lastDate)
-
-*/
-
 const day = document.querySelector('.day')
 const hour = document.querySelector('.hour')
 const minute = document.querySelector('.minute')
@@ -87,20 +25,21 @@ function convertML(dataOne,dataTwo){
     
 }
 
-const firstDate = new Date('2025-05-01T05:26:00');
+const firstDate = new Date('2025-05-04T05:26:00');
 const lastDate = new Date('2026-04-23T12:30:45');
+
 
 convertML(firstDate,lastDate)
 
 
 function countdown(target){
 
-    const invilid = setInterval(()=> {
+    const interval = setInterval(()=> {
 
         let current = new Date()
 
-        const remaining = convertML(target,current)
-    
+        const remaining = convertML(current,target)
+
         day.textContent = remaining.days
     
         hour.textContent = remaining.hours
@@ -110,9 +49,16 @@ function countdown(target){
         second.textContent = remaining.seconds
 
 
-        if(remaining.days <= 0 && remaining.hours <= 0 && remaining.minutes <= 0  && remaining.seconds <= 0){
-            clearInterval(invilid)
+        if(target.getTime() - current.getTime() <= 0){
+            clearInterval(interval)
 
+            day.textContent = '0'
+
+            hour.textContent = '0'
+
+            minute.textContent = '0'
+
+            second.textContent = '0'
         }
 
     },1000)
